f5cc5a8e150e904f54088e5e4cadc89d32ab1a20
Guido Lange <guidolange90@gmail.com>
2023-05-31 20:55:21 +0200

    Add prettier and configure tslint properly

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
 
-function App () {
-  const [count, setCount] = useState(0)
+function App() {
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <div className='flex justify-center items-center'>
+      <div className="flex justify-center items-center">
         <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
@@ -26,18 +26,14 @@ function App () {
       </div>
       <h1>Vite + React 18 + TS + TailwindCSS + ESLint</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the logos to learn more</p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;

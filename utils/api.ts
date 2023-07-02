698ea7a1138b003a4e26fdacf8b7102e5ecf4ac8
Guido Lange <guidolange90@gmail.com>
2023-07-02 22:27:50 +0200

    fix: use exposed public vercel URL in production

--- utils/api.ts
+++ utils/api.ts
@@ -1,9 +1,15 @@
-const baseUrl = `${process.env.NEXT_PUBLIC_URL}/api`;
+export const getBaseUrl = () => {
+  if (process.env.NODE_ENV === 'development') {
+    return `${process.env.NEXT_PUBLIC_URL}/api`;
+  }
+
+  return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api`;
+};
 
 type Method = 'GET' | 'POST' | 'PUT' | 'DELETE';
 
 export const doApiCall = (url: string, method: Method, data?: unknown) =>
-  fetch(`${baseUrl}${url}`, {
+  fetch(`${getBaseUrl()}${url}`, {
     method,
     headers: {
       'Content-Type': 'application/json',

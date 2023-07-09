06042b4949969822eee5c66e62cd793154d71c6a
Guido Lange <Guido.Lange@deutschebahn.com>
2023-07-09 18:48:39 +0200

    description in wish can also be null

--- types/wish.ts
+++ types/wish.ts
@@ -1,7 +1,7 @@
 export type Wish = {
   id?: number | null;
   title: string;
-  description?: string;
+  description?: string | null;
   url?: null | string;
   imageUrl?: null | string;
   giver?: null | string;

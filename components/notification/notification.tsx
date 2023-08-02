76659db504fec0f4b7d992e4b98d0924cf0078ff
Guido Lange <guidolange90@gmail.com>
2023-08-02 21:00:43 +0200

    improve colors for notification

--- components/notification/notification.tsx
+++ components/notification/notification.tsx
@@ -9,9 +9,9 @@ type NotificationProps = Omit<ComponentPropsWithoutRef<'div'>, 'id'> & {
   onDismiss: (id: number) => void;
 };
 
-const typeClassNames = {
-  error: 'text-red-500 bg-red-100 dark:bg-red-800 dark:text-red-200',
-  success: 'text-green-500 bg-green-100 dark:bg-green-800 dark:text-green-200',
+const iconTypeClassNames = {
+  error: 'bg-red-500 dark:bg-red-800',
+  success: 'bg-green-500 dark:bg-green-800',
 };
 const notificationIcon = {
   error: X,
@@ -50,7 +50,7 @@ export const Notification = ({
     <div
       {...props}
       className={clsx(
-        'flex items-center w-full max-w-sm p-4 text-black bg-white rounded-lg shadow dark:text-white dark:bg-gray-900 opacity-0 transition-transform',
+        'flex items-center w-full max-w-sm p-4 text-black bg-white rounded-lg shadow dark:text-white dark:bg-gray-800 opacity-0 transition-transform border border-gray-200 dark:border-gray-700 ',
         open ? 'animate-fade-in-left' : 'animate-fade-out-right',
         className,
       )}
@@ -60,17 +60,17 @@ export const Notification = ({
     >
       <div
         className={clsx(
-          'inline-flex items-center self-start justify-center flex-shrink-0 w-5 h-5 rounded-full',
-          typeClassNames[type],
+          'inline-flex items-center self-start justify-center flex-shrink-0 w-5 h-5 rounded-full text-white dark:text-gray-800',
+          iconTypeClassNames[type],
         )}
       >
-        <Icon className="w-3 h-3" />
+        <Icon className="w-4 h-4 stroke-[3px]" />
       </div>
       <div className="ml-3 text-sm font-normal">{children}</div>
       {type !== 'error' && (
         <button
           type="button"
-          className="ml-auto -mx-1.5 -my-1.5 flex-shrink-0 self-start bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-900 dark:hover:bg-gray-700"
+          className="ml-auto -mx-1.5 -my-1.5 flex-shrink-0 self-start bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
           data-dismiss-target="#toast-success"
           aria-label="Close"
           onClick={handleClose}

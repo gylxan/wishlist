8634865df8d012aed907eb1cdf6e72421d8d8500
Guido Lange <guidolange90@gmail.com>
2023-08-06 22:18:17 +0200

    improve session storage

--- hooks/useSessionStorage.ts
+++ hooks/useSessionStorage.ts
@@ -3,18 +3,17 @@ import {
   SessionStorageKey,
   setSessionStorageItem,
 } from '../utils/session-storage';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 type UseSessionStorageOptions = {
   key: SessionStorageKey;
   fallback?: string | null;
 };
 const useSessionStorage = ({ key, fallback = null }: UseSessionStorageOptions) => {
-  const [storageValue, setValue] = useState(fallback);
-
-  useEffect(() => {
-    setValue(getSessionStorageItem(key));
-  }, [key]);
+  const [storageValue, setValue] = useState(() => {
+    const sessionValue = getSessionStorageItem(key);
+    return sessionValue || fallback;
+  });
 
   const setStorageValue = (value: string) => {
     setSessionStorageItem(key, value);
